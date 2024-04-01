52390b594ef48cb395b3f1cfbf5009d47b3c6d77
Miixter <96525671+Miixter@users.noreply.github.com>
2024-04-01 17:34:34 +0200

    Add files via upload

---
+++ Ingredients.py
@@ -0,0 +1,30 @@
+#Per 100g:
+#Namn: |Kcal, Fett, Kolhydrat, Protein] (g)
+#kycklingbröstfile:[120, 2, 0, 23]
+Ingredient_dict = {
+    "kycklingbröstfile": [120, 2, 0, 23],
+    "lövbiff": [110, 1.6, 0, 24],
+    "lök": [39, 0, 9, 1.1],
+    "vitlök": [160, 0, 31.1, 6.4],
+    "tomatpure": [91, 1, 14, 4.8],
+    "dijonsenap": [151, 11, 3.5, 7],
+    "minifräsch": [80, 5, 4.5, 3.9],
+    "penne": [359, 2, 71, 13],
+    "nötfärs": [190, 12, 0, 19],
+    "spaghetti": [354, 1.3, 73, 11],
+    "krossadetomater": [26, 0, 3.9, 1.2],
+    "morot": [41, 0, 10, 0.9],
+    "balsamvinäger": [127, 0, 27, 0],
+    "ksoja": [150, 0, 30.9, 5.5],
+    "ägg": [147, 10, 0.8, 12.6],
+    "kvarg": [60, 0, 4.4, 9.5],
+
+}
+#Näringsvärden per måltid
+#Namn: [Kcal, Fett, Kolhydrat, Protein] (g)
+Måltider_dict = {
+    "köttfärssåsspaghetti": [664.78, 16.36, 87.23, 37.23],
+    "laxäggnudlarteriyaki": [721.03, 20.77, 88.43, 42.14],
+    "biffstroganoff": [586, 7 ,46.6] #4personer: 500g lövbiff, 2 lökar, 2vitlöksklyftor, 3msk tomatpure, 2dl minifräsch, 1msk dijonsenap, 400g penne, 4dl vatten, kryddor!
+    
+}
---
+++ NutritionalValue.py
@@ -0,0 +1,96 @@
+import Ingredients
+
+def calc_ingredients(ingredienser):
+    kcal = 0
+    fett = 0
+    kolhydrat = 0
+    protein = 0
+    list = ingredienser.split(" ")
+    for element in list:
+        for ingredienser in Ingredients.Ingredient_dict:
+            element_separation = element.split(",")
+            if element_separation[0].lower() == ingredienser:
+                kcal = kcal + float(Ingredients.Ingredient_dict[ingredienser][0])/100*float(element_separation[1])
+                fett = fett + float(Ingredients.Ingredient_dict[ingredienser][1])/100*float(element_separation[1])
+                kolhydrat = kolhydrat + float(Ingredients.Ingredient_dict[ingredienser][2])/100*float(element_separation[1])
+                protein = protein + float(Ingredients.Ingredient_dict[ingredienser][3])/100*float(element_separation[1])
+    return [kcal, fett, kolhydrat, protein]
+
+def calc_meals(måltider):
+    kcal = 0
+    fett = 0
+    kolhydrat = 0
+    protein = 0
+    list = måltider.split(" ")
+    for element in list:
+        for ingredienser in Ingredients.Måltider_dict:
+            if element.lower() == ingredienser:
+                kcal = kcal + Ingredients.Måltider_dict[ingredienser][0]
+                fett = fett + Ingredients.Måltider_dict[ingredienser][1]
+                kolhydrat = kolhydrat + Ingredients.Måltider_dict[ingredienser][2]
+                protein = protein + Ingredients.Måltider_dict[ingredienser][3]
+    return [kcal, fett, kolhydrat, protein]
+
+
+def welcome():
+    print("***")
+    print("Välkommen. Med detta skript kan du räkna ut ditt dagliga intag av kalorier, fett, kolhydrater och protein!")
+    print("***")
+    print("Vi sätter igång direkt!")
+    print(" ")
+    run()
+
+def run():    
+    print("Ange ifall du vill lägga in ingredienser direkt, eller välja en redan färdigkomponerad måltid. För att avsluta programmet, skriv 'exit'. ")
+    message = input("Skriv antingen M eller I: ")
+    print("***")
+    handle_input(message)
+    
+def handle_input(message):
+    if message == "I":
+        list2 = [0,0,0,0]
+        ingredienser = input("Skriv upp samtliga ingredienser som du använt i din måltid, följt av antalet gram. Exempelvis 'kvarg,100 rostmackor,75':\n")
+        message2 = input("Vill du lägga till måltider också? Y/N:")
+        if message2 == "Y":
+            måltider = input("Skriv de måltider du vill lägga till. Exempelvis 'köttfärssås carbonara': \n")
+            list2 = calc_meals(måltider)
+        list1 = calc_ingredients(ingredienser)
+        total_values = [list1[0]+list2[0], list1[1]+list2[1], list1[2]+list2[2], list1[3]+list2[3]]
+        print("***")
+        print(f"antal kalorier: {total_values[0]} \nantal gram fett: {total_values[1]} \nantal gram kolhydrater: {total_values[2]} \nantal gram protein: {total_values[3]}")
+        if sum(total_values) == 0:
+            print("Hmm, 0 på allt verkar fel. Se till att inputen har rätt syntax, och att varje namn finns med i ingrediens- eller måltidsdictionaryt!")
+        print("***")
+        run()
+        
+    
+    elif message == "M":
+        list2 = [0,0,0,0]
+        måltider = input("Skriv de måltider du vill lägga till. Exempelvis 'köttfärssås carbonara': \n")
+        message2 = input("Vill du lägga till ingredienser också? Y/N:")
+        if message2 == "Y":
+            ingredienser = input("Skriv de inredienser du vill lägga till. Exempelvis 'kvarg,100 ägg,100': \n")
+            list2 = calc_ingredients(ingredienser)
+        list1 = calc_meals(måltider)
+        total_values = [list1[0]+list2[0], list1[1]+list2[1], list1[2]+list2[2], list1[3]+list2[3]]
+        print("***")
+        print("DITT INTAG AV NÄRINGSÄMNEN FÖR DET DU HAR MATAT IN:")
+        print(f"antal kalorier: {total_values[0]} g \nantal gram fett: {total_values[1]} g \nantal gram kolhydrater: {total_values[2]} g \nantal gram protein: {total_values[3]} g")
+        if sum(total_values) == 0:
+            print("Hmm, 0 på allt verkar fel. Se till att inputen har rätt syntax, och att varje namn finns med i ingrediens- eller måltidsdictionaryt!")
+        print("***")
+        run()
+    
+    elif message == "exit":
+        quit()
+    
+    else:
+        print("***")
+        message3 = input("Otillåten input. SKriv antingen 'I', 'M', eller 'exit' för att avsluta programmet: ")
+        handle_input(message3)
+
+
+def main():
+    welcome()
+
+main()
